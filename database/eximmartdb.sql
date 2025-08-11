3852554607500b7dea75e89312c46f3d4fbb87fc
nafiz-swe <nafiznoyon480@gmail.com>
2025-08-12 01:04:57 +0600

    db

--- database/eximmartdb.sql
+++ database/eximmartdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 10, 2025 at 09:25 PM
+-- Generation Time: Aug 11, 2025 at 09:03 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -56,6 +56,8 @@ CREATE TABLE `imported_products` (
   `id` int(11) NOT NULL,
   `category` varchar(100) NOT NULL,
   `product_name` varchar(255) NOT NULL,
+  `brand_name` varchar(255) DEFAULT NULL,
+  `product_color` varchar(100) DEFAULT NULL,
   `description` text DEFAULT NULL,
   `price` decimal(10,2) NOT NULL,
   `stored_in` varchar(255) DEFAULT NULL,
@@ -77,10 +79,10 @@ CREATE TABLE `imported_products` (
 -- Dumping data for table `imported_products`
 --
 
-INSERT INTO `imported_products` (`id`, `category`, `product_name`, `description`, `price`, `stored_in`, `stock`, `sold`, `factory_shop_address`, `contact_number`, `product_owner_name`, `product_image1`, `product_image2`, `product_image3`, `product_video`, `import_date`, `created_at`, `updated_at`) VALUES
-(0, 'Computers & Laptops', 'Macbook air', 'test descrptions', 23450.00, '122 location', 333, 5656, 'Xianung', '+8443444674', 'zisimpung', 'images/products/computers-laptops/monitor1.jpg', 'images/products/computers-laptops/2021-637571985004009642-400.jpg', 'images/products/computers-laptops/pc4.jpg', 'images/products/computers-laptops/mhn23.mp4', '2025-08-10', '2025-08-10 09:34:51', '2025-08-10 09:34:51'),
-(0, 'Computers & Laptops', 'dell laptop', 'google img descri', 89.00, '', 258294, 92773, 'Guanzhu', '+4529097732', 'Xuangvung', 'images/products/computers-laptops/deco.jpg', 'images/products/computers-laptops/laptop1.jpg', NULL, NULL, '2025-08-10', '2025-08-10 10:00:12', '2025-08-10 10:00:12'),
-(0, 'Fashion & Wear', 'Man forman dress Court and pant and tie', 'About this item\r\nPrintable cardstock lets you design and print your own personalized business cards\r\nOur most premium business cards feature Clean Edge(R) technology so that cards snap apart easily and leave behind the smoothest cleanest edges of any printable business card available\r\nOptimized for inkjet printers for jam & smudge-free performance guaranteed. Add crisp text and vibrant images to both sides of the professional business cards.\r\nPersonalize your white business card with your own design or choose from thousands of free templates and designs on the Avery site\r\nIdeal for professional-quality printable business cards gift cards gift tags coupons or customer loyalty cards; use the back for photos maps and notes\r\n   Report an issue with this product or seller', 8393.00, '', 4892391, 287892, 'Kunming', '+834710923', 'Kungxing', 'images/products/fashion-wear/general-1.png', 'images/products/fashion-wear/MEN_S_BUSINESS_CASUAL_ATTIRE_1.jpg', 'images/products/fashion-wear/Untitled_design_39_800x.webp', 'images/products/fashion-wear/de.mp4', '2025-08-10', '2025-08-10 10:05:21', '2025-08-10 10:05:21');
+INSERT INTO `imported_products` (`id`, `category`, `product_name`, `brand_name`, `product_color`, `description`, `price`, `stored_in`, `stock`, `sold`, `factory_shop_address`, `contact_number`, `product_owner_name`, `product_image1`, `product_image2`, `product_image3`, `product_video`, `import_date`, `created_at`, `updated_at`) VALUES
+(1, 'Computers & Laptops', 'Macbook air', 'Apache', 'Black, Green, Yellow', 'test descrptions', 23450.00, '122 location', 333, 5656, 'Xianung', '+8443444674', 'zisimpung', 'images/products/computers-laptops/monitor1.jpg', 'images/products/computers-laptops/2021-637571985004009642-400.jpg', 'images/products/computers-laptops/pc4.jpg', 'images/products/computers-laptops/mhn23.mp4', '2025-08-10', '2025-08-10 09:34:51', '2025-08-11 16:22:56'),
+(2, 'Computers & Laptops', 'dell laptop', 'Not applicable', 'White, Blue, Red, Perple', 'google img descri', 89.00, '', 258294, 92773, 'Guanzhu', '+4529097732', 'Xuangvung', 'images/products/computers-laptops/deco.jpg', 'images/products/computers-laptops/laptop1.jpg', NULL, NULL, '2025-08-10', '2025-08-10 10:00:12', '2025-08-11 16:23:22'),
+(3, 'Fashion & Wear', 'Man forman dress Court and pant and tie', 'Ducati', 'Not aplicable', 'About this item\r\nPrintable cardstock lets you design and print your own personalized business cards\r\nOur most premium business cards feature Clean Edge(R) technology so that cards snap apart easily and leave behind the smoothest cleanest edges of any printable business card available\r\nOptimized for inkjet printers for jam & smudge-free performance guaranteed. Add crisp text and vibrant images to both sides of the professional business cards.\r\nPersonalize your white business card with your own design or choose from thousands of free templates and designs on the Avery site\r\nIdeal for professional-quality printable business cards gift cards gift tags coupons or customer loyalty cards; use the back for photos maps and notes\r\n   Report an issue with this product or seller', 8393.00, '', 4892391, 287892, 'Kunming', '+834710923', 'Kungxing', 'images/products/fashion-wear/general-1.png', 'images/products/fashion-wear/MEN_S_BUSINESS_CASUAL_ATTIRE_1.jpg', 'images/products/fashion-wear/Untitled_design_39_800x.webp', 'images/products/fashion-wear/de.mp4', '2025-08-10', '2025-08-10 10:05:21', '2025-08-11 16:23:34');
 
 -- --------------------------------------------------------
 
@@ -126,6 +128,12 @@ ALTER TABLE `delivery`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `imported_products`
+--
+ALTER TABLE `imported_products`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
@@ -149,6 +157,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `delivery`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `imported_products`
+--
+ALTER TABLE `imported_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `orders`
